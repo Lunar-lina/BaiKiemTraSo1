3a6fd591f4ef7670d5e7b345282840dc6a38cb17
Lunar-lina <haihoanmine@gmail.com>
2025-04-07 21:55:08 +0700

    Add files via upload

--- Dashboard.php
+++ Dashboard.php
@@ -1,12 +1,12 @@
 <?php require('./includes/Header.php')?>
-		   <img src="images/6876899.jpg" style="z-index:-1;margin-top:-10px;margin-bottom:-150px;border-radius: 0px 0px 10px 10px;object-fit: cover;" height='900px' width='100%' max-width='100%' max-height='100%' id="target-section" />
-		   <div class="Content" style="text-align: center;margin-top: -410px;">
+<div style="position: relative;">
+		   <img src="images/6876899.jpg" style="z-index:-1;border-radius: 0px 0px 10px 10px;object-fit: cover;" height='900px' width='100%' max-width='100%' max-height='100%' id="target-section" />
+		   <div class="Content" style="text-align:center;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);">
 			<h1 style="background-color: #111111;border: 0px solid #000">Dashboard</h1>
-	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder" ><a>Amounts of Products</a><br><a>0</a></div></button>
-	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder" ><a>Users</a><br><a>0</a></div></button>
-	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder" ><a>Orders</a><br><a>0</a></div></button><br>
+	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder;margin:10px" ><a>Amounts of Products</a><br><a>0</a></div></button>
+	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder;margin:10px" ><a>Users</a><br><a>0</a></div></button>
+	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href=''" style="padding:30px 100px 30px 100px;font-weight:bolder;margin:10px" ><a>Orders</a><br><a>0</a></div></button><br>
 	<button style="border: #000 solid 0px; background-color: #00000000;"><div class="interest" onclick="window.location.href='Products index.php'" style="font-weight:bolder;margin-left:auto;margin-right:auto;margin-top:10px;padding:20px 40px 20px 40px" ><a>Manage Products</a></div></button>
 </div>
-	
-		<br><br><br>
+</div>
 <?php require('./includes/Footer.php')?>
